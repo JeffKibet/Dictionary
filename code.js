7279082d5a95bda426bb4ee4dbfe772eea018260
jeff <jefferykibet@gmail.com>
2026-07-17 15:53:13 +0300

    Add initial JavaScript code to grab essential page elements

---
+++ code.js
@@ -0,0 +1,7 @@
+// Grab all the elements we need from the page
+const searchForm = document.getElementById("searchForm");
+const searchInput = document.getElementById("searchInput");
+const resultsDiv = document.getElementById("results");
+const savedList = document.getElementById("savedList");
+const darkModeBtn = document.getElementById("darkModeBtn");
+const audioPlayer = document.getElementById("audioPlayer");
